20931c97aca72380cceb682c1885e35aebea8192
Suleman Thaniana <msuleman@mit.edu>
2021-08-25 17:53:41 -0400

    Projects described, class names added

--- src/containers/Classes.js
+++ src/containers/Classes.js
@@ -8,18 +8,40 @@ const Classes = () => {
     <div class="center">
     <h1>Classes</h1>
     </div>
-        <GenericClass name="6.115 - Microcomputer Project Lab" Semester="Spring 2021" pt1="Class focused on the basics of microcontrollers" pt2="There were 5
-        labs focused on building systems like a calculator, self driving car and then finally there was a final project" pt3="Technologies used and taught were
-        the C language, assembly, inner runnings of Microprocessors and "/>
-        <GenericClass name="6.003" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
-        <GenericClass name="6.046" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
-        <GenericClass name="6.002" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
-        <GenericClass name="6.004" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
-        <GenericClass name="6.006" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
-        <GenericClass name="6.009" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
-        <GenericClass name="6.08" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
-        <GenericClass name="" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
-        <GenericClass name="" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
+        <GenericClass name="6.115 - Microcomputer Project Lab" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
+        {/* // pt1= "Class focused on the basics of microcontrollers" pt2="There were 5
+        // labs focused on building systems like a calculator, self driving car and then finally there was a final project" pt3="Technologies used and taught were
+        // the C language, assembly, inner runnings of Microprocessors and "/> */}
+        <GenericClass name="6.003 - Signal Processing" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
+        <GenericClass name="6.046 - Design and Analysis of Algorithms" Semester="Spring 2021" pt1="" pt2="" pt3=""/>
+        <GenericClass name="6.002 - Circuits and Electronics" Semester="Fall 2020" pt1="" pt2="" pt3=""/>
+        <GenericClass name="6.004 - Computation Structures" Semester="Fall 2020" pt1="" pt2="" pt3=""/>
+        <GenericClass name="6.006 - Introduction to Algorithms" Semester="Fall 2020" pt1="" pt2="" pt3=""/>
+        <GenericClass name="6.009 - Fundamentals of Programming" Semester="Fall 2020" pt1="" pt2="" pt3=""/>
+        <GenericClass name="6.08 - Intro to EECS via Interconnected Embedded Systems" Semester="Spring 2020" pt1="" pt2="" pt3=""/>
+        <GenericClass name="2.00B - Toy Product Design" Semester="Spring 2020" pt1="" pt2="" pt3=""/>
+        <GenericClass name="6.0001 - Intro to CS Prog in Python" Semester="Fall 2019" pt1="" pt2="" pt3=""/>
+        <GenericClass name="6.0002 - Intro: Comp Thinking & Data Science" Semester="Fall 2019" pt1="" pt2="" pt3=""/>
+        <GenericClass name="6.148 - Web Lab: Prog and Competition" Semester="IAP 2021" pt1="" pt2="" pt3=""/>
+        <GenericClass name="6.176 - Pokerbots Competition" Semester="IAP 2021" pt1="" pt2="" pt3=""/>
+        <GenericClass name="16.632 - Intro to Autonomous Machines " Semester="Fall 2020" pt1="" pt2="" pt3=""/>
+
+
+    <h1>Other Classes</h1>
+    <ul>
+        <li>8.01 - Physics 1</li>
+        <li>8.02 - Physics 2</li>
+        <li>18.01 - Calculus 1</li>
+        <li>18.02 - Calculus 2</li>
+        <li>7.012 - Biology</li>
+        <li>5.111 - Chemistry</li>
+        <li>5.12 - Organic Chemistry 1</li>
+        <li>10.10 - Introduction to CHemical Engineering</li>
+        <li>14.01 - Microeconomics</li>
+        <li>14.02 - Macroeconomics</li>
+        <li>17.41 - International Relations</li>
+        <li>Elementary Arabic (taken at Harvard Divinity Schhool)</li>
+    </ul>
 
 
     </div>
--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -10,11 +10,16 @@ const Projects = () => {
         <div class='AllProjects'>
         <Genericproject name='Create Intervals' link='https://createintervals.herokuapp.com/' photo='intervals.png' describe='This 
         project was created for the web lab class and competition alongside two other students. The goal of the project is 
-        to help people create schedules.'></Genericproject>
-        <Genericproject name='PSoC Game Console' link='https://www.youtube.com/watch?v=-BzoDejxh7I' photo='PSOCGames.png'></Genericproject>
-        <Genericproject name='Fun Simon' link='https://translucent-opaque-twilight.glitch.me/' photo='Simon.png'></Genericproject>
-        <Genericproject name='SnESPChat' photo='Snespchat.png'></Genericproject>
-        <Genericproject name='Shark Attack' photo='sharkattack.jpg'></Genericproject>
+        to help people create schedules. The website is programmed using React. '></Genericproject>
+        <Genericproject name='PSoC Game Console' link='https://www.youtube.com/watch?v=-BzoDejxh7I' photo='PSOCGames.png' describe="A game console 
+        programmed on the PSoC5 microprocessor in C, alongside preparing the internal and external circuitry for the system. The games that can currently be played are 
+        Tic Tac Toe, Simon and an MP4 player."></Genericproject>
+        <Genericproject name='Fun Simon' link='https://translucent-opaque-twilight.glitch.me/' photo='Simon.png' describe="Built the Simon game with a small change. Rather 
+        than colors I added animals and their sounds. The website is made using Javascript, HTML and CSS."></Genericproject>
+        <Genericproject name='SnESPChat' photo='Snespchat.png' describe="Made as part of the final project of the class 6.08. The deivce is a phone like device which 
+        uses an ESP32 to communicate with the sensors on board like the camera and also communicate with an online server via wifi and HTTP methods."></Genericproject>
+        <Genericproject name='Shark Attack' photo='sharkattack.jpg' describe="A fun game made for children. The goal of the game is to get the board across
+        a rotating board with sharks on it. The project was made as part of the class 2.00b which was held remotely. "></Genericproject>
         </div>
     </div>
 }
